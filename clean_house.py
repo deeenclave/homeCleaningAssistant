2b0eaa762e6e2994cfd304da62a6d4a18993b4c2
rangarajl <rangaraj.l@gmail.com>
2019-05-12 15:36:59 -0400

    main helper to clean house and configuration for bedroom cleaning

---
+++ clean_house.py
@@ -0,0 +1,50 @@
+'''''
+Main helper to clean house
+'''''
+
+
+def clean_master_bedroom():
+    group_clothes()
+
+def group_clothes():
+    group_ranga_clothes()
+    group_jaanu_clothes()
+    group_akshu_clothes()
+
+def group_ranga_clothes():
+    group_tops()
+    group_bottoms()
+    group_accesories()
+
+# These are tshirts which will be used only inside
+def fix_tshirt_indoor_spot():
+    print("Fixed t-shirt indoors spot")
+
+# These will be used outdoors and for ocassions
+def fix_tshirt_outdoor_spot():
+    print("Fixed t-shirt indoors spot")
+
+
+def group_tops():
+    print()
+
+def group_jaanu_clothes():
+    print()
+
+def group_akshu_clothes():
+    print()
+
+def group_bottoms():
+    print()
+
+def group_accesories():
+    print()
+
+def fold_clothes():
+    print("folded clothes")
+
+def roll_clothes(spot):
+    print("rolled clothes")
+
+def stack_clothes(spot):
+    print("stacked clothes")
